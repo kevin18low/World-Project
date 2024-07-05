3a5a021658d7aea0ec5d300c44e0fab1aa9ea9e9
Kevin <klow05@student.ubc.ca>
2024-07-05 13:08:41 -0700

    Add map and text field

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+    res.render("index.ejs", {
+        input: req.body.username
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
