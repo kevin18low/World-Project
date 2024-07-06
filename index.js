799cc38b2f0d63474c4b2b2f4925c23f6e8460a0
Kevin <klow05@student.ubc.ca>
2024-07-06 16:29:55 -0700

    Restart and correct answer counter added

--- index.js
+++ index.js
@@ -24,27 +24,43 @@ async function getGuessedCountries() {
     return countries;
 }
 
+async function getTotalCountries() {
+    const result = await db.query("SELECT country_name FROM countries");
+    return result.rows.length;
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
     const countries = await getGuessedCountries();
-    console.log(countries);
+    const total = await getTotalCountries();
     res.render("index.ejs", {
-        countries: countries
+        countries: countries,
+        guessed: countries.length,
+        total: total
     });
 });
 
 app.post("/submit", async (req, res) => {
-    const result = await db.query(
-        "SELECT country_code FROM countries WHERE country_name LIKE '%' || $1 || '%'",
-        [req.body.country]
-    );
-    const country = result.rows[0].country_code;
-    await db.query("INSERT INTO guessed_countries (country) VALUES ($1)", [country]);
+    try {
+        const result = await db.query(
+            "SELECT country_code FROM countries WHERE country_name LIKE '%' || $1 || '%'",
+            [req.body.country]
+        );
+        const country = result.rows[0].country_code;
+        await db.query("INSERT INTO guessed_countries (country) VALUES ($1)", [country]);
+        res.redirect("/");
+    } catch (err) {
+        console.error("Error executing query", err.stack);
+    }
+});
+
+app.post("/reset", async (req, res) => {
+    await db.query("TRUNCATE guessed_countries");
     res.redirect("/");
-})
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+});
