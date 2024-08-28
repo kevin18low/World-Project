2ca24f4dcafcf1a97705017945e34410731ffa41
Kevin <klow05@student.ubc.ca>
2024-08-28 14:35:14 -0700

    Added .env and gitignore

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+env.config();
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "world",
-    password: "**",
-    port: 5432
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PW,
+    port: process.env.PG_PORT,
 });
 
 db.connect();
